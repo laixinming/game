33b6fc747a7658da83ce36ada3a871e073eae0c1
laixinming <127327989+laixinming@users.noreply.github.com>
2026-02-12 23:46:09 +0800

    12

--- core/Combat.js
+++ core/Combat.js
@@ -1,9 +1,11 @@
 import { nft } from "./NFT.js";
 import { web3eth } from "./Web3.js";
+
 export class Combat {
   fight(mapId) {
     const caller = "GAME_UI_TRIGGER";
-    const owner = web3eth.account || "local";
+    // 游客使用本地身份
+    const owner = web3eth.account || "local_guest";
     const tid = nft.mint(owner, mapId, caller);
     if(!tid) return {success:false};
     const equip = erc721.all().find(x=>x.tokenId===tid);
