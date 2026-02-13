9fd6d8530023ff767cd789ee6d14c4601742920b
laixinming <127327989+laixinming@users.noreply.github.com>
2026-02-13 12:54:39 +0800

    123
    Removed unused variable 'caller' and updated mint function call.

--- core/Combat.js
+++ core/Combat.js
@@ -1,13 +1,11 @@
 import { nft } from "./NFT.js";
 import { web3eth } from "./Web3.js";
+import { erc721 } from "./ERC721.js";
 
 export class Combat {
   fight(mapId) {
-    const caller = "GAME_UI_TRIGGER";
-    // 游客使用本地身份
     const owner = web3eth.account || "local_guest";
-    const tid = nft.mint(owner, mapId, caller);
-    if(!tid) return {success:false};
+    const tid = nft.mint(owner, mapId);
     const equip = erc721.all().find(x=>x.tokenId===tid);
     return {success:true, equip};
   }
