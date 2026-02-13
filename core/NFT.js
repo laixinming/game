09078fe36a46f4aed60b80348179c7eeb8314f2a
laixinming <127327989+laixinming@users.noreply.github.com>
2026-02-13 13:29:37 +0800

    123

--- core/NFT.js
+++ core/NFT.js
@@ -1,7 +1,16 @@
 import { Config } from "./Config.js";
 import { erc721 } from "./ERC721.js";
 export class NFT {
-  randomDrop(mapId) { const m=Config.maps.find(x=>x.id==mapId); const mon=m.monsters[Math.random()*m.monsters.length|0]; const eid=mon.dropEquipIds[Math.random()*mon.dropEquipIds.length|0]; return Config.equipments.find(x=>x.id==eid); }
-  mint(owner,mapId,caller) { return erc721.mint(owner, this.randomDrop(mapId), caller); }
+  randomDrop(mapId) {
+    const m = Config.maps.find(x => x.id == mapId);
+    const mon = m.monsters[Math.floor(Math.random() * m.monsters.length)];
+    const eid = mon.dropEquipIds[Math.floor(Math.random() * mon.dropEquipIds.length)];
+    const equip = Config.equipments.find(x => x.id == eid);
+    // 修复：保证返回的装备一定有 type
+    return { ...equip, type: equip.type || "武器" };
+  }
+  mint(owner, mapId) {
+    return erc721.mint(owner, this.randomDrop(mapId));
+  }
 }
 export const nft = new NFT();
